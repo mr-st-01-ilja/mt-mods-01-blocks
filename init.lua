c758ad55c33bc23f2fc40c3fee13be912c833e8f
petrukhnov <konstantin.petrukhnov@gmail.com>
2019-04-14 20:51:57 +0300

    template

---
+++ init.lua
@@ -0,0 +1,71 @@
+--numbers
+--1
+minetest.register_node('mrblock:1', {
+	tile_images = {'1.png'},
+	inventory_image = '1.png',
+	sunlight_propagates = true,
+	paramtype = 'light',
+	walkable = true,
+	climbable = false,
+	diggable = true,
+	drawtype = "nodebox",
+	groups = { choppy = 3, oddly_breakable_by_hand = 1},
+	material = minetest.digprop_constanttime(1.0),
+	description = "1"
+})
+-- 2
+minetest.register_node('mrblock:2', {
+	tile_images = {'2.png'},
+	inventory_image = '2.png',
+	sunlight_propagates = true,
+	paramtype = 'light',
+	walkable = true,
+	climbable = false,
+	diggable = true,
+	drawtype = "nodebox",
+	groups = { choppy = 3, oddly_breakable_by_hand = 1},
+	material = minetest.digprop_constanttime(1.0),
+	description = "2"
+})
+-- 3
+minetest.register_node('mrblock:3', {
+	tile_images = {'3.png'},
+	inventory_image = '3.png',
+	sunlight_propagates = true,
+	paramtype = 'light',
+	walkable = true,
+	climbable = false,
+	diggable = true,
+	drawtype = "nodebox",
+	groups = { choppy = 3, oddly_breakable_by_hand = 1},
+	material = minetest.digprop_constanttime(1.0),
+	description = "3"
+})
+--4
+minetest.register_node('mrblock:4', {
+	tile_images = {'4.png'},
+	inventory_image = '4.png',
+	sunlight_propagates = true,
+	paramtype = 'light',
+	walkable = true,
+	climbable = false,
+	diggable = true,
+	drawtype = "nodebox",
+	groups = { choppy = 3, oddly_breakable_by_hand = 1},
+	material = minetest.digprop_constanttime(1.0),
+	description = "4"
+})
+--5
+minetest.register_node('mrblock:5', {
+	tile_images = {'5.png'},
+	inventory_image = '5.png',
+	sunlight_propagates = true,
+	paramtype = 'light',
+	walkable = true,
+	climbable = false,
+	diggable = true,
+	drawtype = "nodebox",
+	groups = { choppy = 3, oddly_breakable_by_hand = 1},
+	material = minetest.digprop_constanttime(1.0),
+	description = "5"
+})
